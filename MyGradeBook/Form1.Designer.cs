e6f12cb3c85834a205f0df8c2f2b3574047e5206
David Lee <davidlee@csu.fullerton.edu>
2014-03-20 12:27:31 -0700

    Minor Changes
    Added more elements

---
+++ MyGradeBook/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MyGradeBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtItem01Name = new System.Windows.Forms.TextBox();
+            this.txtItem01Earned = new System.Windows.Forms.TextBox();
+            this.txtItem01Possible = new System.Windows.Forms.TextBox();
+            this.txtItem02Possible = new System.Windows.Forms.TextBox();
+            this.txtItem02Earned = new System.Windows.Forms.TextBox();
+            this.txtItem02Name = new System.Windows.Forms.TextBox();
+            this.txtItem03Possible = new System.Windows.Forms.TextBox();
+            this.txtItem03Earned = new System.Windows.Forms.TextBox();
+            this.txtItem03Name = new System.Windows.Forms.TextBox();
+            this.btnClass01 = new System.Windows.Forms.Button();
+            this.btnClass02 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.txtItem01Grade = new System.Windows.Forms.TextBox();
+            this.txtItem01Weight = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtItem01WeightedGrade = new System.Windows.Forms.TextBox();
+            this.btnClassNames = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(0, 0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtItem01Name
+            // 
+            this.txtItem01Name.Location = new System.Drawing.Point(134, 78);
+            this.txtItem01Name.Name = "txtItem01Name";
+            this.txtItem01Name.Size = new System.Drawing.Size(100, 20);
+            this.txtItem01Name.TabIndex = 1;
+            // 
+            // txtItem01Earned
+            // 
+            this.txtItem01Earned.Location = new System.Drawing.Point(241, 78);
+            this.txtItem01Earned.Name = "txtItem01Earned";
+            this.txtItem01Earned.Size = new System.Drawing.Size(35, 20);
+            this.txtItem01Earned.TabIndex = 2;
+            // 
+            // txtItem01Possible
+            // 
+            this.txtItem01Possible.Location = new System.Drawing.Point(282, 78);
+            this.txtItem01Possible.Name = "txtItem01Possible";
+            this.txtItem01Possible.Size = new System.Drawing.Size(35, 20);
+            this.txtItem01Possible.TabIndex = 3;
+            // 
+            // txtItem02Possible
+            // 
+            this.txtItem02Possible.Location = new System.Drawing.Point(282, 104);
+            this.txtItem02Possible.Name = "txtItem02Possible";
+            this.txtItem02Possible.Size = new System.Drawing.Size(35, 20);
+            this.txtItem02Possible.TabIndex = 6;
+            // 
+            // txtItem02Earned
+            // 
+            this.txtItem02Earned.Location = new System.Drawing.Point(241, 104);
+            this.txtItem02Earned.Name = "txtItem02Earned";
+            this.txtItem02Earned.Size = new System.Drawing.Size(35, 20);
+            this.txtItem02Earned.TabIndex = 5;
+            // 
+            // txtItem02Name
+            // 
+            this.txtItem02Name.Location = new System.Drawing.Point(134, 104);
+            this.txtItem02Name.Name = "txtItem02Name";
+            this.txtItem02Name.Size = new System.Drawing.Size(100, 20);
+            this.txtItem02Name.TabIndex = 4;
+            // 
+            // txtItem03Possible
+            // 
+            this.txtItem03Possible.Location = new System.Drawing.Point(282, 130);
+            this.txtItem03Possible.Name = "txtItem03Possible";
+            this.txtItem03Possible.Size = new System.Drawing.Size(35, 20);
+            this.txtItem03Possible.TabIndex = 9;
+            // 
+            // txtItem03Earned
+            // 
+            this.txtItem03Earned.Location = new System.Drawing.Point(241, 130);
+            this.txtItem03Earned.Name = "txtItem03Earned";
+            this.txtItem03Earned.Size = new System.Drawing.Size(35, 20);
+            this.txtItem03Earned.TabIndex = 8;
+            // 
+            // txtItem03Name
+            // 
+            this.txtItem03Name.Location = new System.Drawing.Point(134, 130);
+            this.txtItem03Name.Name = "txtItem03Name";
+            this.txtItem03Name.Size = new System.Drawing.Size(100, 20);
+            this.txtItem03Name.TabIndex = 7;
+            // 
+            // btnClass01
+            // 
+            this.btnClass01.Location = new System.Drawing.Point(23, 78);
+            this.btnClass01.Name = "btnClass01";
+            this.btnClass01.Size = new System.Drawing.Size(75, 23);
+            this.btnClass01.TabIndex = 10;
+            this.btnClass01.Text = "CLASS 1";
+            this.btnClass01.UseVisualStyleBackColor = true;
+            this.btnClass01.Click += new System.EventHandler(this.btnClass01_Click);
+            // 
+            // btnClass02
+            // 
+            this.btnClass02.Location = new System.Drawing.Point(23, 106);
+            this.btnClass02.Name = "btnClass02";
+            this.btnClass02.Size = new System.Drawing.Size(75, 23);
+            this.btnClass02.TabIndex = 11;
+            this.btnClass02.Text = "CLASS 2";
+            this.btnClass02.UseVisualStyleBackColor = true;
+            this.btnClass02.Click += new System.EventHandler(this.btnClass02_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Item";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Earned";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(279, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Possible";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(113, 81);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Done";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(113, 107);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(113, 133);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 18;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // txtItem01Grade
+            // 
+            this.txtItem01Grade.Location = new System.Drawing.Point(323, 78);
+            this.txtItem01Grade.Name = "txtItem01Grade";
+            this.txtItem01Grade.Size = new System.Drawing.Size(35, 20);
+            this.txtItem01Grade.TabIndex = 19;
+            // 
+            // txtItem01Weight
+            // 
+            this.txtItem01Weight.Location = new System.Drawing.Point(364, 78);
+            this.txtItem01Weight.Name = "txtItem01Weight";
+            this.txtItem01Weight.Size = new System.Drawing.Size(35, 20);
+            this.txtItem01Weight.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(320, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Grade";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(363, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Weight";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(401, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Weighted Grade";
+            // 
+            // txtItem01WeightedGrade
+            // 
+            this.txtItem01WeightedGrade.Location = new System.Drawing.Point(405, 78);
+            this.txtItem01WeightedGrade.Name = "txtItem01WeightedGrade";
+            this.txtItem01WeightedGrade.Size = new System.Drawing.Size(35, 20);
+            this.txtItem01WeightedGrade.TabIndex = 24;
+            // 
+            // btnClassNames
+            // 
+            this.btnClassNames.Location = new System.Drawing.Point(563, 0);
+            this.btnClassNames.Name = "btnClassNames";
+            this.btnClassNames.Size = new System.Drawing.Size(75, 23);
+            this.btnClassNames.TabIndex = 25;
+            this.btnClassNames.Text = "Names";
+            this.btnClassNames.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 407);
+            this.Controls.Add(this.btnClassNames);
+            this.Controls.Add(this.txtItem01WeightedGrade);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtItem01Weight);
+            this.Controls.Add(this.txtItem01Grade);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClass02);
+            this.Controls.Add(this.btnClass01);
+            this.Controls.Add(this.txtItem03Possible);
+            this.Controls.Add(this.txtItem03Earned);
+            this.Controls.Add(this.txtItem03Name);
+            this.Controls.Add(this.txtItem02Possible);
+            this.Controls.Add(this.txtItem02Earned);
+            this.Controls.Add(this.txtItem02Name);
+            this.Controls.Add(this.txtItem01Possible);
+            this.Controls.Add(this.txtItem01Earned);
+            this.Controls.Add(this.txtItem01Name);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtItem01Name;
+        private System.Windows.Forms.TextBox txtItem01Earned;
+        private System.Windows.Forms.TextBox txtItem01Possible;
+        private System.Windows.Forms.TextBox txtItem02Possible;
+        private System.Windows.Forms.TextBox txtItem02Earned;
+        private System.Windows.Forms.TextBox txtItem02Name;
+        private System.Windows.Forms.TextBox txtItem03Possible;
+        private System.Windows.Forms.TextBox txtItem03Earned;
+        private System.Windows.Forms.TextBox txtItem03Name;
+        private System.Windows.Forms.Button btnClass01;
+        private System.Windows.Forms.Button btnClass02;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox txtItem01Grade;
+        private System.Windows.Forms.TextBox txtItem01Weight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtItem01WeightedGrade;
+        private System.Windows.Forms.Button btnClassNames;
+    }
+}
+
---
+++ MyGradeBook/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGradeBook
+{
+    public partial class Form1 : Form
+    {
+        int selection = 0;
+
+        string[] class01ItemNames = new string[3];
+        double[] class01PointsEarned = new double[3];
+        double[] class01PointsPossible = new double[3];
+
+        string[] class02ItemNames = new string[3];
+        double[] class02PointsEarned = new double[3];
+        double[] class02PointsPossible = new double [3];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (selection == 1)
+            {
+                class01ItemNames[0] = Convert.ToString(txtItem01Name.Text);
+                class01ItemNames[1] = Convert.ToString(txtItem02Name.Text);
+                class01ItemNames[2] = Convert.ToString(txtItem03Name.Text);
+
+                class01PointsEarned[0] = Convert.ToDouble(txtItem01Earned.Text);
+                class01PointsEarned[1] = Convert.ToDouble(txtItem02Earned.Text);
+                class01PointsEarned[2] = Convert.ToDouble(txtItem03Earned.Text);
+
+                class01PointsPossible[0] = Convert.ToDouble(txtItem01Earned.Text);
+                class01PointsPossible[1] = Convert.ToDouble(txtItem02Earned.Text);
+                class01PointsPossible[2] = Convert.ToDouble(txtItem03Earned.Text);
+            }
+            else if (selection == 2)
+            {
+                class02ItemNames[0] = Convert.ToString(txtItem01Name.Text);
+                class02ItemNames[1] = Convert.ToString(txtItem02Name.Text);
+                class02ItemNames[2] = Convert.ToString(txtItem03Name.Text);
+
+                class02PointsEarned[0] = Convert.ToDouble(txtItem01Earned.Text);
+                class02PointsEarned[1] = Convert.ToDouble(txtItem02Earned.Text);
+                class02PointsEarned[2] = Convert.ToDouble(txtItem03Earned.Text);
+
+                class02PointsPossible[0] = Convert.ToDouble(txtItem01Earned.Text);
+                class02PointsPossible[1] = Convert.ToDouble(txtItem02Earned.Text);
+                class02PointsPossible[2] = Convert.ToDouble(txtItem03Earned.Text);
+            }
+        }
+
+        private void btnClass01_Click(object sender, EventArgs e)
+        {
+            selection = 1;
+            txtItem01Name.Text = class01ItemNames[0];
+            txtItem02Name.Text = class01ItemNames[1];
+            txtItem03Name.Text = class01ItemNames[2];
+
+            txtItem01Earned.Text = Convert.ToString(class01PointsEarned[0]);
+            txtItem02Earned.Text = Convert.ToString(class01PointsEarned[1]);
+            txtItem03Earned.Text = Convert.ToString(class01PointsEarned[2]);
+
+            txtItem01Possible.Text = Convert.ToString(class01PointsPossible[0]);
+            txtItem02Possible.Text = Convert.ToString(class01PointsPossible[1]);
+            txtItem03Possible.Text = Convert.ToString(class01PointsPossible[2]);
+        }
+
+        private void btnClass02_Click(object sender, EventArgs e)
+        {
+            selection = 2;
+            txtItem01Name.Text = class02ItemNames[0];
+            txtItem02Name.Text = class02ItemNames[1];
+            txtItem03Name.Text = class02ItemNames[2];
+
+            txtItem01Earned.Text = Convert.ToString(class02PointsEarned[0]);
+            txtItem02Earned.Text = Convert.ToString(class02PointsEarned[1]);
+            txtItem03Earned.Text = Convert.ToString(class02PointsEarned[2]);
+
+            txtItem01Possible.Text = Convert.ToString(class02PointsPossible[0]);
+            txtItem02Possible.Text = Convert.ToString(class02PointsPossible[1]);
+            txtItem03Possible.Text = Convert.ToString(class02PointsPossible[2]);
+        }
+    }
+}
